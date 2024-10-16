61a6d0a9be92a3a2937f9fc09a9417abfc675f49
José Thomaz <jose.thoomaz@gmail.com>
2024-10-15 22:39:55 -0300

    Encontrar dois

--- src/main/java/org/example/EncontrarDois.java
+++ src/main/java/org/example/EncontrarDois.java
@@ -1,4 +1,35 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EncontrarDois {
+
+    List<Integer> lista = new ArrayList<>();
+
+    public int numerodedois(List<Integer> lista) {
+        int count = 0;
+
+        for (Integer num : lista) {
+            if (num == 2) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        EncontrarDois encontrarDois = new EncontrarDois();
+
+        encontrarDois.lista.add(1);
+        encontrarDois.lista.add(2);
+        encontrarDois.lista.add(3);
+        encontrarDois.lista.add(2);
+        encontrarDois.lista.add(5);
+        encontrarDois.lista.add(2);
+
+        int resultado = encontrarDois.numerodedois(encontrarDois.lista);
+        System.out.println("Quantidade de números 2: " + resultado);
+    }
 }
